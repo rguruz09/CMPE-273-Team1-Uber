880bb0da59a80a8a31fe0c75a45b3496b8f27c75
rguruz09 <raghavendraramakrishnaiah.guru@sjsu.edu>
2015-11-30 11:35:37 -0800

    Sample mocha test cases

---
+++ UberRabbitMQClient/test_cases/testcases1.js
@@ -0,0 +1,18 @@
+var supertest = require("supertest");
+var should = require("should");
+var server = supertest.agent("http://localhost:3005/");
+
+describe("First test", function(){
+	it("return home page", function(done){
+		server
+			.get("/")
+			.expect(200)
+			.end(function(err, res){
+				console.log("Res "+res);
+				should(res.status).equal(200);		
+				done();
+			})	
+	});
+});
+
+
